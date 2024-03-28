11790c13054e97de259f918c0a2856a7e6696554
zain <zainkazmi258@gmail.com>
2024-03-28 13:42:36 +0500

    functionlity completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 import { useState } from 'react'
 import Box from './components/box'
 import './App.css'
+import "./assets/pexels-breakingpic-3299.jpg"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div className="flex">
+      <div className='w-full h-screen bg-black relative flex flex-col items-center'>
 
-    
-      <Box/>
-      
+        <div className="image h-1/3 overflow-hidden flex items-center relative">
+          <img className='w-full object-cover absolute' src="https://blog.teamup.com/wp-content/uploads/2021/12/todo-header.png" alt="" />
+           {/* <div className="absolute w-full h-full z-10 opacity-50 bg-lime-950"></div> */}
+        </div>
+        <div className="h-2/3 grid grid-cols-4 absolute top-80 rounded-t-lg w-full bg-slate-500 z-20">
+
+
+          <div className="grid-item flex justify-center items-center"><Box color = "bg-indigo-800" heading= "TODAY"/></div>
+          <div className="grid-item flex justify-center items-center"><Box color = "bg-rose-800" heading="THIS WEEK" /></div>
+          <div className="grid-item flex justify-center items-center"><Box color = "bg-fuchsia-800" heading="THIS YEAR" /></div>
+          <div className="grid-item flex justify-center items-center"><Box color= "bg-green-800" heading="LONG TERM" /></div>
+
+ 
+
+        </div>
       </div>
     </>
   )
--- src/components/box.jsx
+++ src/components/box.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
-const Box = () => {
+const Box = ({color , heading }) => {
+
     const [hide, sethide] = useState(true);
     const [hide2, sethide2] = useState(false);
     const [hide3, sethide3] = useState(true);
@@ -9,6 +10,7 @@ const Box = () => {
     const [checkedItems, setCheckedItems] = useState([]);
     const [editValue, setEditValue] = useState("");
     const [old, setold] = useState("")
+    
 
     const addclick = () => {
         sethide(!hide);
@@ -28,8 +30,10 @@ const Box = () => {
     };
 
     const handleCheckboxChange = (event) => {
-        const { value, checked } = event.target;
-        if (checked) {
+        
+        const { value } = event.target; // Get the value of the checkbox input
+        const isChecked = event.target.checked;
+        if (isChecked) {
             setCheckedItems([...checkedItems, value]);
         } else {
             setCheckedItems(checkedItems.filter(item => item !== value));
@@ -63,27 +67,29 @@ const Box = () => {
       
 
     useEffect(() => {
-        const storedTodos = JSON.parse(localStorage.getItem('todos'));
+        const storedTodos = (localStorage.getItem(`todos_${heading}_${color}`));
         if (storedTodos) {
-          settodo(storedTodos);
+          settodo(JSON.parse(storedTodos));
         }
     }, []);
     
     useEffect(() => {
-        localStorage.setItem('todos', JSON.stringify(todos));
+        // Check if todos array is not empty before saving to local storage
+        if (todos.length > 0) {
+            localStorage.setItem(`todos_${heading}_${color}`, JSON.stringify(todos));
+        }
     }, [todos]);
-
     return (
-        <div className="bg-slate-600 w-80 h-96 relative rounded-xl">
+        <div className={`${color} w-5/6 h-96 relative rounded-xl flex`}>
             <div className="bg-slate-950 absolute h-20 w-full rounded-t-xl text-cyan-50 flex items-center justify-center">
                 <div className="buttons bg-slate-700 h-12 w-4/5 rounded-lg flex justify-evenly items-center border-2 ">
                     <form id='add' className={`${hide ? "hidden" : ""} ${hide3 ? "" : "hidden"}  flex w-full justify-evenly`} onSubmit={handlesubmit}>
-                        <input className="h-3/4 w-2/3 text-black" type="text" value={inputValue} onChange={handlechange} />
+                        <input className="h-3/4 w-2/3 text-black" type="text" id='add' value={inputValue} onChange={handlechange} />
                         <button type="submit">Add</button>
                         <button onClick={addclick}>X</button>
                     </form>
                     <form id='edit' className={`${hide3 ? "hidden" : ""} flex w-full justify-evenly`} onSubmit={submitedit}>
-                        <input className="h-3/4 w-2/3 text-black" type="text" value={editValue} onChange={(e) => setEditValue(e.target.value)} />
+                        <input className="h-3/4 w-2/3 text-black" id='edit' type="text" value={editValue} onChange={(e) => setEditValue(e.target.value)} />
                         <button type="submit">Edit</button>
                         <button onClick={() => {sethide(true); sethide3(true)}} >X</button>
                     </form>
@@ -94,9 +100,11 @@ const Box = () => {
                     <button className={`${hide2 ? "" : "hidden"}`} onClick={deltask}>DELETE</button>
                 </div>
             </div>
-            <div className="text-white absolute top-20 w-full">
-                <h1>Must do :</h1>
-                <ul className={`${hide2 ? "hidden" : ""} w-full flex flex-col items-center`}>
+            <div className="text-white absolute top-20 w-full flex flex-col items-center h-full">
+                <div className="heading h-1/5 flex items-center">
+                <h1 className='border-2'>{heading}</h1>
+                </div>
+                <ul className={`${hide2 ? "hidden" : ""} w-full flex flex-col items-center h-4/5`}>
                     {todos.map((item, index) => (
                         <li className='flex justify-between w-5/6 border-2' key={index}>
                             <p>{item}</p>
@@ -104,10 +112,10 @@ const Box = () => {
                         </li>
                     ))}
                 </ul>
-                <ul className={`${hide2 ? "" : "hidden"}`}>
+                <ul className={`${hide2 ? "" : "hidden"} w-3/4 flex flex-col items-start`}>
                     {todos.map((item, index) => (
                         <li key={index}>
-                            <input type="checkbox" id={item} name="" value={item} onChange={handleCheckboxChange} />
+                            <input type="checkbox" id={`${color+index}`} value={item} onChange={handleCheckboxChange} />
                             <label htmlFor={item}>{item}</label>
                         </li>
                     ))}

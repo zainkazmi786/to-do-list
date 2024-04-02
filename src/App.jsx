59b4520ebbfd46c9784758b4fcabca1e34627ede
zain <zainkazmi258@gmail.com>
2024-04-02 23:43:20 +0500

    font

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function App() {
   const todo = ["T", "o", "-", "D", "o", " ", "L", "i", "s", "t"];
   const [renderedTodo, setRenderedTodo] = useState('');
   const [index, setIndex] = useState(0);
-  
+
   useEffect(() => {
-    
+
     const timer = setInterval(() => {
       if (index < todo.length) {
         setRenderedTodo(prevRenderedTodo => prevRenderedTodo + todo[index]);
@@ -20,14 +20,14 @@ function App() {
     }, 500);
     return () => clearInterval(timer);
   }, [todo]);
-  
+
   return (
     <>
       <div className='w-full h-screen relative flex flex-col overflow-hidden bg-black gap-20'>
 
-        <div className="lg:h-1/3 w-full flex items-center justify-center">
-          <div className="textblock h-2/3">
-          <span className="head font-extrabold font-sans text-with-shadow"> {renderedTodo} </span>
+        <div className=" w-full flex items-center justify-center">
+          <div className="textblock flex justify-center items-center w-full">
+            <span className="head tracking-widest font-extrabold font-sans text-with-shadow h-1/3"> {renderedTodo} </span>
 
             <span className='head font-extrabold font-mono text-with-shadow'>_</span>
           </div>
